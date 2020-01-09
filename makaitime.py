00d0feb6665e5d169133b2dcd4cf2d6850009312
Prajwol Lamichhane <prajwollamichhane11@gmail.com>
2020-01-09 15:03:00 +0545

    Nothing to Commit removal of a file

--- makaitime.py
+++
@@ -1,8 +0,0 @@
-import time
-import datetime
-from datetime import  fromtimestamp
-
-timestamp = [1554742548133, 1554742538950, 1554742269461, 1554742256643, 1554742248905, 1554741992898, 1554741986025, 1554741983584, 1554741873162, 1554741862026]
-print(type(timestamp))
-date = datetime.fromtimestamp(timestamp[0])
-print(date)
